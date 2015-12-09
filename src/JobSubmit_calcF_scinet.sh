ad1db8bb88dd86740b3d0f43358a8a8d07276432
fgoebels <florian.goebels@gmail.com>
2015-12-09 11:34:04 -0500

    Del outfiles

--- src/JobSubmit_calcF_scinet.sh
+++ src/JobSubmit_calcF_scinet.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-#PBS -l nodes=1:ppn=8,walltime=24:00:00
+#PBS -l nodes=1:ppn=8,walltime=00:20:00
 #PBS -N calcF-Candid
+#PBS -q debug
 
 module load intel/15.0.2 R/3.1.1 python/2.7.8
 
 cd /home/g/gbader/fgoebels/mouseMap
+
+echo $INPUT
+echo $OUTPUT
 
 src/Calcscores.sh Yeast/data/Yeast_240.txt src/TCSS/gene_ontology.obo.txt Yeast/data/gene_association.tab $INPUT $OUTPUT
